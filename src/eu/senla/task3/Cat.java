046e7d4788d255e6242c144b378d2030e91d6ac6
Dzmitry <dkandrusik@gmail.com>
2021-01-27 21:35:27 +0300

    update task3

--- src/eu/senla/task3/Cat.java
+++ src/eu/senla/task3/Cat.java
@@ -2,14 +2,49 @@ package eu.senla.task3;
 
 public class Cat {
 
-    public Cat(String Kitty, int i) {
+    String catName = "Gucci";
+    String catAge = "2y.o";
+
+    private String name2;
+    private int age2;
+
+    public String name = "Vasia";
+    public int age = 9;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
-    public void catDoWhile() {
-        int i=0;
-        Cat cat3 = new Cat("Kitty", 5);
-        do {
-            System.out.println("name " + cat3.name + " age " + cat3.age);
+    public Cat() { //Не понял, зачем предложило добавить
+
+    }
+
+
+
+   public  void task1() {
+        int count = 0;
+
+        while (count < 10) {
+            System.out.println("Name: " + catName + ", Age: " + catAge);
+            count++;
         }
-        while (i++ < 9);
+    }
+
+
+    public String getName2() {
+        return name2;
+    }
+
+    public void setName2(String name2) {
+        this.name2 = name2;
+    }
+
+    public int getAge2() {
+        return age2;
+    }
+
+    public void setAge2(int age2) {
+        this.age2 = age2;
+    }
 }
--- src/eu/senla/task3/Main.java
+++ src/eu/senla/task3/Main.java
@@ -1,8 +1,49 @@
 package eu.senla.task3;
 
+import eu.senla.task3.Cat;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Cat Cat = new Cat("Matilda", 5);
+
+        Cat.task1(); //task 1.
+
+        Cat.setAge2(6); //task 2
+        Cat.setName2("Emmy");
+        String catName = Cat.getName2();
+        int catAge = Cat.getAge2();
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Name: " + catName + ", Age: " + catAge + "y.o");
+        }
+
+        int index = 0; //task 3
+        do {
+            index++;
+            System.out.println("Name: " + Cat.name + ", Age: " + Cat.age);
+        } while (index < 10);
+
+
+
+        Cat.setName2("Belik"); //task 4
+        Cat.setAge2(3);
+        int[] cotik = new int[4];
+        for (int i : cotik) {
+            System.out.println("Name: " + Cat.getName2() + ", Age: " + Cat.getAge2());
+        }
+
+        Cat Cat1 = new Cat (); //task 3.1
+        Cat Cat2 = new Cat ();
+        Cat1.name = "Anton";
+        Cat2.name = "Anton";
+        Cat1.age = 8;
+        Cat2.age = 10;
+        System.out.println(Cat1 == Cat2);
+        System.out.println(Cat1.name.equals(Cat2.name));
     }
+
+
+
+
 }
